99b82df72c1857d74edaa98dbce70c18547d1650
Rich2 <rr1@tutanota.com>
2023-09-10 00:30:50 +0100

    Created some html.

--- src/Main.scala
+++ src/Main.scala
@@ -1,5 +1,15 @@
 import jakarta.servlet.http.{HttpServlet, HttpServletRequest => HSReq, HttpServletResponse => HSResp}
 
-object ServApp extends App{
-  println("Hello Servlet.")
+object ServApp extends HttpServlet
+{
+  def html =
+    """<body>
+      |<h1>This is the start!</h1>
+      |<p>This is the body.</p>
+      |</body>
+      |""".stripMargin
+  override def doGet(req: HSReq, resp: HSResp): Unit =
+  {
+    resp.getWriter().print(html)
+  }
 } 
