a3c582c687c897e7cf7ed755bb2cec8cfb083c3e
Rich2 <rr1@tutanota.com>
2023-09-19 02:20:44 +0100

    Added state.

--- src/Main.scala
+++ src/Main.scala
@@ -3,15 +3,18 @@ import jakarta.servlet.http.{HttpServlet, HttpServletRequest => HSReq, HttpServl
 
 class ServApp extends HttpServlet
 {
+  var numReqs: Int = 0
   def html(str: String): String =
     s"""<body>
       |<h1>This is the start!</h1>
       |<p>This is some info. $str</p>
+      |<p>This is requst number $numReqs.</p>
       |</body>
       |""".stripMargin
   override def doGet(req: HSReq, resp: HSResp): Unit =
   {
     val str = req.getMethod
+    numReqs += 1
     resp.getWriter().println(html(str))
   }
 }
