858e58d3aca779b6ba985e6e95dbc13545b8697e
Rich2 <rr1@tutanota.com>
2023-09-19 01:27:36 +0100

    Minor.

--- src/Main.scala
+++ src/Main.scala
@@ -3,14 +3,15 @@ import jakarta.servlet.http.{HttpServlet, HttpServletRequest => HSReq, HttpServl
 
 class ServApp extends HttpServlet
 {
-  def html: String =
-    """<body>
+  def html(str: String): String =
+    s"""<body>
       |<h1>This is the start!</h1>
-      |<p>This is the body.</p>
+      |<p>This is some info. $str</p>
       |</body>
       |""".stripMargin
   override def doGet(req: HSReq, resp: HSResp): Unit =
   {
-    resp.getWriter().println(html)
+    val str = req.getMethod
+    resp.getWriter().println(html(str))
   }
 }
